efffa54b99571d77a38180f19b9baf76a712be0e
Devin Cooper <devinraycooper@gmail.com>
2025-08-05 05:31:58 -0400

    add debug controller

---
+++ src/main/java/space/devincoopers/cdnserver/controller/DebugController.java
@@ -0,0 +1,21 @@
+package space.devincoopers.cdnserver.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/**")
+public class DebugController {
+
+    @GetMapping
+    public ResponseEntity<String> logRequest(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        System.out.println("➡️ Incoming Request URI: " + path);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Requested: " + path);
+    }
+}
